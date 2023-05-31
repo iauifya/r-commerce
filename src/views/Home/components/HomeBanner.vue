7243141b27fb290a85d41deb6f86d2d97e974307
柯姿伶 <Sophia.Ke@homeplus.net.tw>
2023-05-31 17:37:18 +0800

    banner 頁面

--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,5 +1,38 @@
+<script setup>
+// import { getBannerAPI } from '@/apis/home.js';
+// import { ref,onMounted } from 'vue'
+// const bannerList = ref([])
+// const getBanner = async() =>{
+//   const res = await getBannerAPI();
+//   console.log(res);
+//   bannerList.value = res.result;
+// }
+// onMounted(()=>{
+//   getBanner()
+// })
+</script>
 <template>
-  <div>
-    &nbsp;我是banner
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3 class="small justify-center" text="2xl">{{ item }}</h3>
+        <!-- <img src="" alt=""> -->
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
+
+<style scoped lang="less">
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
+  img{
+    width: 100%;
+    height: 500px;
+  }
+}
+</style>
