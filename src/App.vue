c9fadb7caf5d24b75751fef46948e0b1a6121189
柯姿伶 <Sophia.Ke@homeplus.net.tw>
2023-05-31 14:56:36 +0800

    app.vue style update

--- src/App.vue
+++ src/App.vue
@@ -7,31 +7,35 @@
   <RouterView /> 
 </template>
 
-<style scoped lang="scss">
-header {
-  line-height: 1.5;
+<style scoped lang="less">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
+// header {
+//   line-height: 1.5;
+// }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+// .logo {
+//   display: block;
+//   margin: 0 auto 2rem;
+// }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+// @media (min-width: 1024px) {
+//   header {
+//     display: flex;
+//     place-items: center;
+//     padding-right: calc(var(--section-gap) / 2);
+//   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+//   .logo {
+//     margin: 0 2rem 0 0;
+//   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+//   header .wrapper {
+//     display: flex;
+//     place-items: flex-start;
+//     flex-wrap: wrap;
+//   }
+// }
 </style>
