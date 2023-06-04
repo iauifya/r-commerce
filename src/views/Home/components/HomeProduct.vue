f7cc86f3ef2f625471463c7d67f44f1c18960d9b
iauifya <iauifya@gmail.com>
2023-06-04 23:07:55 +0800

    goodsitem組件封裝

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HomePanel from './HomePanel.vue';
+import GoodsItem from './GoodsItem.vue';
 import { getGoodsAPI } from '@/apis/home.js';
 import { ref, onMounted } from 'vue';
 const goodsList = ref([]);
@@ -26,20 +27,7 @@ onMounted(() => {
         </router-link>
         <ul class="goods-list">
           <li v-for="good in cate.goods" :key="good.id">
-            <div class="goods-item">
-              <RouterLink :to="`/goods/${good.id}`" class="image">
-                <img :src="good.picture" src="" :alt="good.name" />
-              </RouterLink>
-              <p class="name ellipsis-2">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-              <div class="extra">
-                <RouterLink to="/">
-                  <span>找相似</span>
-                  <span>发现现多宝贝 &gt;</span>
-                </RouterLink>
-              </div>
-            </div>
+            <GoodsItem :good="good"></GoodsItem>
           </li>
         </ul>
       </div>
@@ -135,82 +123,6 @@ onMounted(() => {
         }
       }
 
-      .goods-item {
-        width: 240px;
-        height: 300px;
-        padding: 10px 30px;
-        position: relative;
-        overflow: hidden;
-        border: 1px solid transparent;
-        transition: all 0.5s;
-
-        .image {
-          display: block;
-          width: 160px;
-          height: 160px;
-          margin: 0 auto;
-
-          img {
-            width: 100%;
-            height: 100%;
-          }
-        }
-
-        p {
-          margin-top: 6px;
-          font-size: 16px;
-
-          &.name {
-            height: 44px;
-          }
-
-          &.desc {
-            color: #666;
-            height: 22px;
-          }
-
-          &.price {
-            margin-top: 10px;
-            font-size: 20px;
-            color: @priceColor;
-          }
-        }
-
-        .extra {
-          position: absolute;
-          left: 0;
-          bottom: 0;
-          height: 86px;
-          width: 100%;
-          background: @xtxColor;
-          text-align: center;
-          transform: translate3d(0, 100%, 0);
-          transition: all 0.5s;
-
-          span {
-            display: block;
-            color: #fff;
-            width: 120px;
-            margin: 0 auto;
-            line-height: 30px;
-
-            &:first-child {
-              font-size: 18px;
-              border-bottom: 1px solid #fff;
-              line-height: 40px;
-              margin-top: 5px;
-            }
-          }
-        }
-
-        &:hover {
-          border-color: @xtxColor;
-
-          .extra {
-            transform: none;
-          }
-        }
-      }
     }
   }
 }
