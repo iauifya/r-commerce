a42630db2cb5bb594b8cb36084ef6d207de621cb
柯姿伶 <Sophia.Ke@homeplus.net.tw>
2023-06-08 17:06:13 +0800

    新增二級分類api

--- src/apis/category.js
+++ src/apis/category.js
@@ -2,10 +2,20 @@ import httpInstance from '@/utils/http'
 
 //一級麵包屑api
 export function getCategoryAPI(id) {
-  return httpInstance({
-      url: '/category',
-      params: {
-        id
-      }
-  })
+    return httpInstance({
+        url: '/category',
+        params: {
+            id
+        }
+    })
+}
+
+//二級麵包屑api
+export function getCategoryFilterAPI(id) {
+    return httpInstance({
+        url: '/category/filter',
+        params: {
+            id
+        }
+    })
 }
